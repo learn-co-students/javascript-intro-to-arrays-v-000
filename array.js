3bd836394752e8c8ba61c59ee692e2fe4acf167a
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-05 19:19:37 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function createArray() {
+  var candyBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return candyBars;
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  array[2] = "Carter";
+  return array[2];
+}
+
+function removeElementFromArray(dishes) {
+  dishes.splice(2, 2, "Roast Chicken")
+  return dishes;
+}
+
+function iterateArray(numbers) {
+  var newNums = [];
+
+  numbers.forEach(function(number) {
+    newNums.push(number + 5);
+  });
+
+  return newNums;
+}
