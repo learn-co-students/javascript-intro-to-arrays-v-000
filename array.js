eedbbec31f42352dd5357d5683678dc4bbfb65bc
rafeeq-123 <rafeeq-123@hotmail.com>
2016-08-22 13:53:42 -0700

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function createArray() {
+  var goodies = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return  goodies;
+
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array;
+
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+
+}
+
+function replaceElementInArray(array) {
+  return array[2] = "Carter";
+  // return array;
+}
+
+function removeElementFromArray(array) {
+  array.splice(2, 2, "Roast Chicken");
+  return array;
+}
+
+function iterateArray(array) {
+  var newNums = [];
+
+  array.forEach(function(oldnumber) {
+    newNums.push(oldnumber + 5)
+  });
+  return newNums;
+}
