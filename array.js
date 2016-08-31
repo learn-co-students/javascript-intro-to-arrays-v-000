875b00d1a66407c6a12493a0c6e8cffa17f795d5
shoko furushita <shktkt219@gmail.com>
2016-08-31 14:58:36 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(movies){
+  movies.push("A Christmas Story");
+  return movies;
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(names){
+  names[1] = "Carter";
+  return names[1];
+}
+
+function removeElementFromArray(dishes){
+  dishes.splice(2, 2);
+  dishes.push("Roast Chicken");
+  return dishes;
+}
+
+function iterateArray(numArr){
+  var newNums = [];
+  numArr.forEach(function(num){
+    newNums.push(num + 5);
+  });
+  return newNums;
+}
