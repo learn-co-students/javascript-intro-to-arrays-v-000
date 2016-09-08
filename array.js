993fcbd2bdd4ae8f723f88ac8479fb7bfd396957
Adam Taitano <adam.taitano@gmail.com>
2016-09-08 17:24:31 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function createArray() {
+  var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return array;
+};
+
+function addElementToArray(array) {
+  var new = array.push("A Christmas Story");
+  return new;
+};
+
+function accessElementFromArray(array) {
+  return array[2];
+};
+
+function replaceElementInArray(array) {
+  var replaced = array[1];
+  array[1] = "Carter";
+  return replaced;
+};
+
+function removeElementFromArray(array) {
+  var new = array.splice(2, 2);
+  var newer = new.push("Roast Chicken");
+  return newer;
+};
