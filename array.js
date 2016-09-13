e54fe852b1ed301d9aba1ac0b8bee9cf9536122d
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-13 16:49:24 -0400

    donzo

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+  var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return array;
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  return array[1] = "Carter";
+}
+
+function removeElementFromArray(array) {
+  array.splice(2, 2, "Roast Chicken");
+  return array;
+}
+
+function iterateArray(array) {
+  var newNums = [];
+  array.forEach(function(num){
+    newNums.push(num + 5)
+  });
+  return newNums;
+}
