f0b22188e82f95a761dd84cc2f07e5577a86eb1c
Michael Chen <michael.chen.1120@gmail.com>
2016-09-20 21:46:09 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function createArray() {
+  var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return array
+};
+
+function addElementToArray(array) {
+  var array = ["Home Alone", "Love Actually", "Elf"];
+  array.push("A Christmas Story");
+  return array;
+};
+
+function accessElementFromArray(array) {
+  var array = ["New York", "California", "Virginia", "Louisiana", "Nevada"];
+  return array[2];
+};
+
+function replaceElementInArray(array) {
+  var array = ["Joe", "Tim", "Stephen", "Sarah", "Stephanie"];
+  array[1] = "Carter"
+  return array[1];
+};
+
+function removeElementFromArray(array) {
+  var array = ["Pasta Alfredo", "Grilled Salmon", "Hamburgers", "Black Bean Soup"];
+  array.splice(2,2,"Roast Chicken");
+  return array;
+};
+
+function iterateArray(array) {
+  var newNums = []
+  array.forEach(function(number){
+    newNums.push(number + 5);
+  });
+  return newNums
+};
