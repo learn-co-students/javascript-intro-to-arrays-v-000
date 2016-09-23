47f05b97da27976f8471a9f6ebaa89c63250ee16
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-09-23 14:53:17 -0700

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray(){
+	var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+	return array;
+}
+
+function addElementToArray(array){
+	array.push("A Christmas Story");
+	return array;
+}
+
+function accessElementFromArray(array){
+	return array[2];
+}
+
+function replaceElementInArray(array){
+	return array[1] = "Carter";
+}
+
+function removeElementFromArray(array){
+	var newArray = array.splice(2, 2, 'Roast Chicken');
+	return array;
+}
+
+function iterateArray(array){
+	var newNums = [];
+	array.forEach(function(letter){
+		newNums.push(letter + 5);
+	})
+	return newNums;
+}
