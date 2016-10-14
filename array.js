9673e20ef135ffc08e144cdf90ce3ec9526c3295
DEREJE GEMECHU <dereje88@gmail.com>
2016-10-14 05:48:55 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function createArray() {
+  var newArray = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return newArray;
+}
+
+function addElementToArray(addToArray) {
+  addToArray.push('A Christmas Story');
+  return addToArray;
+}
+
+function accessElementFromArray(accArray) {
+  return accArray[2];
+
+}
+
+function replaceElementInArray(repArray) {
+  repArray[2] = "Carter";
+  return repArray[2];
+}
+
+function removeElementFromArray(removArray) {
+  removArray.splice(2,2);
+  removArray.push("Roast Chicken");
+  return removArray;
+}
+
+function iterateArray(iterArray) {
+  var count = 0;
+  do {
+    iterArray.push(iterArray.length + ++count);
+    iterArray.shift();
+
+  } while(count < 5);
+
+  return iterArray;
+}
