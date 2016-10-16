7f93c354c24390a0f25701e1dc5586a07b5cf45a
SiamKing <thesiameseelectric@gmail.com>
2016-10-15 17:52:07 -0700

    what the func

--- array.js
+++ array.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  return array[1] = "Carter";
+}
+
+function removeElementFromArray(array) {
+  array.splice(2, 2, "Roast Chicken");
+  return array;
+}
+
+function iterateArray(array) {
+  var newNums = new Array();
+  array.forEach(function(num, index) {
+    newNums[index] = array[index] + 5;
+  })
+  return newNums;
+}
