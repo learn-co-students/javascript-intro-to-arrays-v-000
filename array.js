32526d50a37c5f6c46e70c7de1bb80a94af0b0ee
Kathleen <kayjsmyth@gmail.com>
2016-10-18 16:51:39 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+	return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(array) {
+	array.push("A Christmas Story");
+	return array;
+}
+
+function accessElementFromArray(array) {
+	return array[2];
+}
+
+function replaceElementInArray(array) {
+	return array[2] = "Carter";
+
+}
+
+function removeElementFromArray(array) {
+	array.splice(2, 2, "Roast Chicken");
+	return array;
+}
+
+function iterateArray(array) {
+	var new_array = [];
+	array.forEach(function(number){
+		new_array.push(number + 5);
+	});
+	return new_array;
+}
