a661f6b0e082ecc4a0800648d4398fcf045b946c
Ann Lee <aplee29@users.noreply.github.com>
2016-10-31 21:23:14 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+  return ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+}
+
+function addElementToArray(movieArr) {
+  movieArr.push("A Christmas Story");
+  return movieArr;
+}
+
+function accessElementFromArray(statesArr) {
+  return statesArr[2];
+}
+
+function replaceElementInArray(namesArr) {
+  namesArr[1] = "Carter";
+  return namesArr[1];
+}
+
+function removeElementFromArray(dishesArr) {
+  dishesArr.splice(2, 2, "Roast Chicken");
+  return dishesArr;
+}
+
+function iterateArray(numArr) {
+  var newNums =[];
+  numArr.forEach(function(num) {
+    newNums.push(num + 5);
+  });
+  return newNums;
+}
