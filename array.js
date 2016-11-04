c6062da7c757c2a9fb534ffe14edf226bbde1c72
Dom McKellar <dom.mckellar@gmail.com>
2016-11-03 18:54:26 -0700

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(arr){
+  arr.push("A Christmas Story");
+  return arr;
+}
+
+function accessElementFromArray(states){
+  return states[2];
+}
+
+function replaceElementInArray(names){
+  return names[1] = "Carter";
+  // return names[1];
+}
+
+function removeElementFromArray(dishes){
+  dishes.splice(2, 2, "Roast Chicken");
+  return dishes;
+}
+
+function iterateArray(myArray){
+  var newNums = [];
+  myArray.forEach(function(num) {
+    newNums.push(num + 5);
+  });
+  return newNums;
+}
