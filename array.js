43f4601d446985350b88695a9f69c58cfa0f11bf
Levio Marotti III <lm3@protonmail.com>
2016-11-16 09:54:32 -0600

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story")
+  return array;
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  array[1] = "Carter"
+  return array[1];
+}
+
+function removeElementFromArray(array) {
+  array.splice(2, 2, "Roast Chicken")
+  return array;
+}
+
+function iterateArray(array) {
+  var newNums = []
+  for (var i = 0; i < array.length; i++) {
+    newNums.push(array[i] + 5)
+  }
+  return newNums;
+}
