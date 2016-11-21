d6772f1e77d0ae0bed73eb4c3a138a0eda4b09cf
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-11-21 13:34:39 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function createArray() {
+  var candybars = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return candybars;
+  }
+
+function addElementToArray(christmasMovies) {
+  var movies = ["Home Alone", "Love Actually", "Elf"];
+  movies.push("A Christmas Story");
+  return movies
+  } 
