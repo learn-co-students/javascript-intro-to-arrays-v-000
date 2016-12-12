3df2f56899d4539a5d803de5d14dca023307f2cd
bshap93 <bshap93@gmail.com>
2016-12-11 18:40:50 -0600

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,31 @@
 'use strict';
+function createArray() {
+	return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+
+function addElementToArray(ary) {
+	ary.push("A Christmas Story");
+	return ary;
+}
+
+function accessElementFromArray(ary) {
+	return ary[2];
+}
+
+function replaceElementInArray(ary) {
+	ary[1] = "Carter";	
+	return ary[1];
+}
+
+function removeElementFromArray(ary) {
+	ary.splice(2,2, "Roast Chicken");
+	return ary;
+}
+function iterateArray(ary) {
+	var newNums = [];
+	ary.forEach(function(number){
+		newNums.push(number + 5);
+	});
+	return newNums;
+}
