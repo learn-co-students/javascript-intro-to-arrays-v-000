df7338ff71dca6663212f1f4dda2c30d0e2e3a33
Jonathan Foster <redcabinny@gmail.com>
2016-12-22 13:32:35 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(arr) {
+  arr.push("A Christmas Story");
+  return arr;
+}
+
+function accessElementFromArray(arr) {
+  return arr[2];
+}
+
+function replaceElementInArray(arr) {
+  return arr[1] = "Carter";
+}
+
+function removeElementFromArray(arr) {
+  arr.splice(2, 2, "Roast Chicken");
+  return arr;
+}
+
+function iterateArray(arr) {
+  var newNums = [];
+  arr.forEach(function(x, y) {
+    newNums[y] = x + 5;
+  })
+  return newNums;
+}
