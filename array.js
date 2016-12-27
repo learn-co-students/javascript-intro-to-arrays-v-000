fa3b2df19fe9d172a457c280f345efc66213b745
AliceKB <alicekristinabalbuena@gmail.com>
2016-12-27 18:13:46 -0500

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story")
+  return array
+}
+
+function accessElementFromArray(stateArray) {
+  return stateArray[2];
+}
+
+function replaceElementInArray(nameArray) {
+  nameArray[2] = "Carter"
+  return nameArray[2];
+}
+
+function removeElementFromArray(dishesArray) {
+  dishesArray.splice(2, 2, "Roast Chicken")
+  return dishesArray
+}
+
+function iterateArray(numArray) {
+  var newNums = []
+  numArray.forEach(function(num) {
+    newNums.push(num + 5)
+  });
+  return newNums;
+}
