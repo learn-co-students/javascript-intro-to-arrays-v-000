e901975e1d73643c2af9f978aca87204d480b152
BriOD <briodonoghue@gmail.com>
2017-01-04 14:48:02 -0500

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(array){
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(states){
+  return states[2];
+}
+
+function replaceElementInArray(names){
+  names[1] = "Carter";
+  return names[1];
+}
+
+function removeElementFromArray(dishes){
+  dishes.splice(2, 2, "Roast Chicken");
+  return dishes;
+}
+
+function iterateArray(nums){
+  var newNums = [];
+  nums.forEach(function(num) {
+    newNums.push(num + 5);
+  });
+  return newNums;
+}
