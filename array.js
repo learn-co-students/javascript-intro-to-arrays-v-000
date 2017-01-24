8b605758a47ffb721fc831035474ff4b44ea0085
github username <github email address>
2017-01-24 00:03:40 -0500

    Done.

--- array.js
+++ array.js
@@ -1,38 +1,32 @@
 'use strict';
 function createArray() {
   var random = new Array();
-  return random = ["snickers", "hundred grand", "kitkat", "skittles"]
+  return random = ["snickers", "hundred grand", "kitkat", "skittles"];
 }
 
-var practice = new Array()
 function addElementToArray(practice) {
-  practice.push("A Christmas Story")
-  return practice
+  practice.push("A Christmas Story");
+  return practice;
 }
 
-var states = new Array()
 function accessElementFromArray(states) {
-  return states[2]
+  return states[2];
 }
 
-var names = new Array();
 function replaceElementInArray(names) {
-  names[1] = "Carter"
-  return names[1]
+  names[1] = "Carter";
+  return names[1];
 }
 
-var dishes = new Array();
 function removeElementFromArray(dishes) {
-  dishes.splice(2, 2, "Roast Chicken")
-  return dishes
+  dishes.splice(2, 2, "Roast Chicken");
+  return dishes;
 }
 
-var numbers = new Array();
 function iterateArray(numbers) {
   var newNums = new Array();
   numbers.forEach(function(number) {
-      //var newNums = new Array()
-      newNums.push( number + 5 )
+      newNums.push( number + 5 );
   });
-  return newNums
+  return newNums;
 };
