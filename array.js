19c223a74f456c576fd441149f9e06b5f8856296
smithWEBtek <brad@smithwebtek.com>
2017-02-14 21:21:43 -0500

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+  var ary = ["snickers", "hundred grand", "kitkat", "skittles"]
+  return ary; 
+}
+
+function addElementToArray(ary){
+  ary.push("A Christmas Story");
+  return ary;
+}
+
+function accessElementFromArray(ary){
+  return ary[2];
+}
+
+function replaceElementInArray(ary){
+  ary[1] = "Carter";
+  return ary[1];
+}
+
+function removeElementFromArray(ary){
+  ary.splice(2, 2, 'Roast Chicken');
+  return ary;
+}
+
+function iterateArray(ary){
+  var newNums = [];
+  ary.forEach(function(num){
+    newNums.push(num+5);
+  });
+  return newNums;
+}
