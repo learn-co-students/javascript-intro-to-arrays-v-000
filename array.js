1d5a6764a27566fde38325f5215d5e0daf8ab152
Brie Coyle <brie.coyle@gmail.com>
2017-05-12 15:41:25 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function createArray() {
+  var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return array;
+}
+ function addElementToArray(array) {
+   array.push("A Christmas Story");
+   return array;
+ }
+function accessElementFromArray(states) {
+  return states[2];
+}
+function replaceElementInArray(names) {
+  names[1] = "Carter";
+  return names[1];
+}
+function removeElementFromArray(dishes) {
+  dishes.splice(2, 2, 'Roast Chicken');
+  return dishes;
+}
+function iterateArray(nums) {
+  var newNums = [];
+  nums.forEach(function(num) {
+    newNums.push(num + 5)
+  })
+  return newNums;
+}
